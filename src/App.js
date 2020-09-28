46423707d36b0fb19e4d16c178e30908420dd43b
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-09-28 11:11:42 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/main.css'
+import Data from './data/etsy.json';
+import Listing from './components/Listing';
+
+const data = Data;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="item-list">
+      <Listing items={data}/>    
     </div>
   );
 }
